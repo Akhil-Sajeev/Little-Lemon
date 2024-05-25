86b601b0613571565e5d6af6b42df0a3793f1fe2
Akhil-Sajeev <akhilsajeev2787@gmail.com>
2024-05-25 13:30:02 +0530

    media querry and script inncluded

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSidebar(){
+    const open = document.getElementById('closeSidebar');
+    open.style.display = 'flex';
+}
+function closeSidebar(){
+    const close = document.getElementById('closeSidebar');
+    close.style.display = 'none';
+}
